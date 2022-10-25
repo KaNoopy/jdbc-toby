dd5da7586bf5044bfd21b06341b87c11fe8ff4d0
kahyun Lee <gmeoqq@gmail.com>
2022-10-25 14:24:38 +0900

    Strategy 인터페이스 분리 ,Add, DeleteAll 구현

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -17,9 +17,10 @@ public class UserDao {
     public void deleteAll() throws SQLException {
         Connection c = null;
         PreparedStatement ps = null;
+
         try {
             c = connectonMaker.makeConnection();
-            ps = c.prepareStatement("delete from user");
+            ps = new DeleteAllStrategy().makePreparedStatement(c);
             ps.executeUpdate();
         } catch (SQLException e) {
             throw e;
@@ -76,11 +77,10 @@ public class UserDao {
 
     public void add(User user) throws SQLException {
         Connection c = connectonMaker.makeConnection();
-        PreparedStatement ps = c.prepareStatement("insert into user(id,name,password) values(?,?,?);");
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
-
+        PreparedStatement ps = new AddStrategy(user).makePreparedStatement(c);
+//        ps.setString(1, user.getId());
+//        ps.setString(2, user.getName());
+//        ps.setString(3, user.getPassword());
         ps.executeUpdate();
         ps.close();
         c.close();
