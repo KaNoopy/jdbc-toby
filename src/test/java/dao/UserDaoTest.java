6821330b209e7adc74bf100aa19d96cce262e969
kahyun Lee <gmeoqq@gmail.com>
2022-10-25 13:14:20 +0900

    spring 적용

--- src/test/java/dao/UserDaoTest.java
+++ src/test/java/dao/UserDaoTest.java
@@ -3,16 +3,26 @@ package dao;
 import domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes= UserDaoFactory.class)
 class UserDaoTest {
+    @Autowired
+    ApplicationContext context;
+
     @Test
     void addAndFindById() throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDaoFactory().localUserDao();
-        String id ="20";
+        UserDao userDao = context.getBean("localUserDao",UserDao.class);
+//        UserDao userDao = new UserDaoFactory().localUserDao();
+        String id ="21";
         User user =new User(id,"kahyun","123456");
         userDao.add(user);
 
